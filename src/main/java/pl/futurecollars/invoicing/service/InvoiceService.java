e3bd591a0c4c6162801e15493e9b5bfbde8faaaa
Dawid Chojnacki <manganian1990@gmail.com>
2022-09-08 10:22:31 +0200

    Deleted Autowired in InvoiceService

--- src/main/java/pl/futurecollars/invoicing/service/InvoiceService.java
+++ src/main/java/pl/futurecollars/invoicing/service/InvoiceService.java
@@ -2,7 +2,6 @@ package pl.futurecollars.invoicing.service;
 
 import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
 import pl.futurecollars.invoicing.db.Database;
 import pl.futurecollars.invoicing.model.Invoice;
 
@@ -10,7 +9,6 @@ public class InvoiceService {
 
   private final Database database;
 
-  @Autowired
   public InvoiceService(Database database) {
     this.database = database;
   }
