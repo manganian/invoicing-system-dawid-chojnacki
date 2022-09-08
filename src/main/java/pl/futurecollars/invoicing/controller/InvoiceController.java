a39c2c6cb6149ecf485ae4ff224794946806afb9
Dawid Chojnacki <88980418+manganian@users.noreply.github.com>
2022-09-08 20:15:24 +0200

    Task 5 dependency injection (#12)
    * File based database implementation and tests
    
    * File based database: changed update invoice method impl and tests
    
    * changed update invoice method impl
    
    * changed update method - one list
    
    * Added Spring Boot and InvoiceController class
    
    * Refactored InvoiceController and InvoiceService
    
    * Refactored
    
    * Changed update method in Invoice Controller
    
    * Added DI and Beans
    
    * Changed Beans Config and deleted PathsConfiguration class
    
    * Changed update and post mapping method response in InvoiceController
    
    * Changed fields on private in InvoiceConfiguration
    
    * Deleted Autowired in InvoiceService

--- src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
+++ src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
@@ -1,6 +1,8 @@
 package pl.futurecollars.invoicing.controller;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +12,19 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.futurecollars.invoicing.db.memory.InMemoryDatabase;
 import pl.futurecollars.invoicing.model.Invoice;
 import pl.futurecollars.invoicing.service.InvoiceService;
 
 @RestController
 @RequestMapping("invoices")
-public class InvoiceController {
+class InvoiceController {
 
-  private final InvoiceService invoiceService = new InvoiceService(new InMemoryDatabase());
+  private final InvoiceService invoiceService;
+
+  @Autowired
+  public InvoiceController(InvoiceService invoiceService) {
+    this.invoiceService = invoiceService;
+  }
 
   @GetMapping
   public ResponseEntity<List<Invoice>> getAll() {
@@ -32,8 +38,7 @@ public class InvoiceController {
 
   @PostMapping
   public ResponseEntity<Integer> saveInvoice(@RequestBody Invoice invoice) {
-    invoiceService.save(invoice);
-    return ResponseEntity.status(201).build();
+    return new ResponseEntity<Integer>(invoiceService.save(invoice), HttpStatus.CREATED);
   }
 
   @DeleteMapping("/{id}")
@@ -48,7 +53,7 @@ public class InvoiceController {
   @PutMapping("/{id}")
   public ResponseEntity<?> update(@PathVariable int id, @RequestBody Invoice invoice) {
     if (invoiceService.update(id, invoice)) {
-      return ResponseEntity.ok().build();
+      return ResponseEntity.noContent().build();
     } else {
       return ResponseEntity.notFound().build();
     }
