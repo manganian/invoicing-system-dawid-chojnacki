88f9d05e113999f761bfac6d94c5cf5932f012ff
Justyna Karolska <justyna.karolska@gmail.com>
2022-09-15 22:00:21 +0200

    Added Springfox, Springfox UI and Swagger UI

---
+++ src/main/java/pl/futurecollars/invoicing/config/SpringFoxConfiguration.java
@@ -0,0 +1,36 @@
+package pl.futurecollars.invoicing.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxConfiguration {
+
+  @Bean
+  public Docket docket() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("pl.futurecollars"))
+        .paths(PathSelectors.any())
+        .build()
+        .tags(
+            new Tag("invoices", "endpoint allowing listing/adding/removing/updating invoices")
+        )
+        .apiInfo(apiInfo());
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+        .description("Application to manage set of invoices")
+        .license("No license available - private")
+        .title("===== FC Invoicing APP =====")
+        .build();
+  }
+}
