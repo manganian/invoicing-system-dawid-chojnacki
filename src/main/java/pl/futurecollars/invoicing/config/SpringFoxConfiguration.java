744ee5dff2d54af59253f5c8e97000233b9e5ee8
Justyna Karolska <justyna.karolska@gmail.com>
2022-09-16 11:46:22 +0200

    Added contact info

--- src/main/java/pl/futurecollars/invoicing/config/SpringFoxConfiguration.java
+++ src/main/java/pl/futurecollars/invoicing/config/SpringFoxConfiguration.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -31,6 +32,12 @@ public class SpringFoxConfiguration {
         .description("Application to manage set of invoices")
         .license("No license available - private")
         .title("===== FC Invoicing APP =====")
+        .contact(
+            new Contact(
+                "Dawid Chojnacki",
+                "https://github.com/manganian",
+                "manganian1990@gmail.com")
+        )
         .build();
   }
 }
