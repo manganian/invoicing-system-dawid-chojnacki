bf4457d26adae9ffbd6dd029b450467d801e9455
Dawid Chojnacki <manganian1990@gmail.com>
2022-09-16 12:20:18 +0200

    Documented model classes and InvoiceController. Added InvoiceApi interface. Added new field - quantity to InvoiceEntry

---
+++ src/main/java/pl/futurecollars/invoicing/controller/InvoiceApi.java
@@ -0,0 +1,39 @@
+package pl.futurecollars.invoicing.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import java.util.List;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.futurecollars.invoicing.model.Invoice;
+
+@RequestMapping ("invoices")
+@Api (tags = {"invoices"})
+public interface InvoiceApi {
+
+  @ApiOperation(value = "Add new invoice to system")
+  @GetMapping(produces = {"application/json;charset=UTF-8"})
+  ResponseEntity<List<Invoice>> getAll();
+
+  @ApiOperation(value = "Get invoice by id")
+  @GetMapping(value = "/{id}", produces = {"application/json;charset=UTF-8"})
+  ResponseEntity<Invoice> getById(@PathVariable int id);
+
+  @ApiOperation(value = "Add new invoice to system")
+  @PostMapping
+  ResponseEntity<Integer> saveInvoice(@RequestBody Invoice invoice);
+
+  @ApiOperation(value = "Delete invoice with given id")
+  @DeleteMapping("/{id}")
+  ResponseEntity<?> delete(@PathVariable int id);
+
+  @ApiOperation(value = "Update invoice with given id")
+  @PutMapping("/{id}")
+  ResponseEntity<?> update(@PathVariable int id, @RequestBody Invoice invoice);
+}
--- src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
+++ src/main/java/pl/futurecollars/invoicing/controller/InvoiceController.java
@@ -1,47 +1,37 @@
 package pl.futurecollars.invoicing.controller;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.futurecollars.invoicing.model.Invoice;
 import pl.futurecollars.invoicing.service.InvoiceService;
 
 @RestController
-@RequestMapping("invoices")
-public class InvoiceController {
+@AllArgsConstructor
+public class InvoiceController implements InvoiceApi {
 
   private final InvoiceService invoiceService;
 
-  @Autowired
-  public InvoiceController(InvoiceService invoiceService) {
-    this.invoiceService = invoiceService;
-  }
-
-  @GetMapping(produces = { "application/json;charset=UTF-8" })
+  @Override
   public ResponseEntity<List<Invoice>> getAll() {
     return ResponseEntity.ok(invoiceService.getAll());
   }
 
-  @GetMapping(value = "/{id}", produces = { "application/json;charset=UTF-8" })
+  @Override
   public ResponseEntity<Invoice> getById(@PathVariable int id) {
     return ResponseEntity.of(invoiceService.getById(id));
   }
 
-  @PostMapping
+  @Override
   public ResponseEntity<Integer> saveInvoice(@RequestBody Invoice invoice) {
     return new ResponseEntity<Integer>(invoiceService.save(invoice), HttpStatus.CREATED);
   }
 
-  @DeleteMapping("/{id}")
+  @Override
   public ResponseEntity<?> delete(@PathVariable int id) {
     if (invoiceService.delete(id)) {
       return ResponseEntity.noContent().build();
@@ -50,7 +40,7 @@ public class InvoiceController {
     }
   }
 
-  @PutMapping("/{id}")
+  @Override
   public ResponseEntity<?> update(@PathVariable int id, @RequestBody Invoice invoice) {
     if (invoiceService.update(id, invoice)) {
       return ResponseEntity.noContent().build();
